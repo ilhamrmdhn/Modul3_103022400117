28c73124b08a578e16a32f3b7205052e9ba5d77b
ilhamrmdhn <ilham712svr@gmail.com>
2026-03-10 08:03:54 +0700

    Menambahkan implementasi konversi suhu

--- Modul3_103022400117/Form1.cs
+++ Modul3_103022400117/Form1.cs
@@ -9,7 +9,49 @@ namespace Modul3_103022400117
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (comboBoxSatuanAwal.SelectedItem == null || comboBoxSatuanAkhir.SelectedItem == null)
+            {
+                MessageBox.Show("Pilih satuan terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
+            double nilaiAwal;
+            if (!double.TryParse(textBoxNilaiAwal.Text, out nilaiAwal))
+            {
+                MessageBox.Show("Masukkan angka yang valid!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string awal = comboBoxSatuanAwal.SelectedItem.ToString();
+            string akhir = comboBoxSatuanAkhir.SelectedItem.ToString();
+            double hasil = 0;
+
+            if (awal == akhir)
+            {
+                hasil = nilaiAwal;
+            } else if (awal == "Celcius")
+            {
+                if (akhir == "Reamur") hasil = (4.0 / 5.0) * nilaiAwal;
+                else if (akhir == "Kelvin") hasil = nilaiAwal + 273;
+                else if (akhir == "Fahrenheit") hasil = (9.0 / 5.0) * nilaiAwal + 32;
+            } else if (awal == "Reamur")
+            {
+                if (akhir == "Celcius") hasil = (5.0 / 4.0) * nilaiAwal;
+                else if (akhir == "Kelvin") hasil = (5.0 / 4.0) * nilaiAwal + 273;
+                else if (akhir == "Fahrenheit") hasil = (9.0 / 4.0) * nilaiAwal + 32;
+            } else if (awal == "Fahrenheit")
+            {
+                if (akhir == "Celcius") hasil = 5.0 / 9.0 * (nilaiAwal - 32);
+                else if (akhir == "Reamur") hasil = 4.0 / 9.0 * (nilaiAwal - 32);
+                else if (akhir == "Kelvin") hasil = 5.0 / 9.0 * (nilaiAwal - 32) + 273;
+            } else if (awal == "Kelvin")
+            {
+                if (akhir == "Celcius") hasil = nilaiAwal - 273;
+                else if (akhir == "Reamur") hasil = 4.0 / 5.0 * (nilaiAwal - 273);
+                else if (akhir == "Fahrenheit") hasil = 9.0 / 5.0 * (nilaiAwal - 273) + 32;
+            }
+
+            textBoxNilaiAkhir.Text = hasil.ToString();
         }
     }
 }
